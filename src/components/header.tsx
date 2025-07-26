3329f586a259fd8cc06b5bf368fdfe2272540421
umesh kumar bedi <umeshkumarbedi@gmail.com>
2025-07-26 18:05:52 +0000

    searching style updated

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,35 +6,37 @@ import { cn } from '@/lib/utils';
 import { SearchIcon } from 'lucide-react';
 
 import { liteClient as algoliasearch } from 'algoliasearch/lite';
-import { InstantSearch, SearchBox, Hits, Highlight, useSearchBox, useInstantSearch, Configure } from 'react-instantsearch';
+import { InstantSearch, SearchBox, Hits, Highlight, useSearchBox, useInstantSearch, Configure, RefinementList } from 'react-instantsearch';
 // import 'instantsearch.css/themes/satellite.css';
 import Image from 'next/image'
 
 const searchClient = algoliasearch(`${process.env.NEXT_PUBLIC_ALGOLIA_APP_ID}`, `${process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_ONLY_API_KEY}`);
 
 const Hit = ({ hit }: any) => (
+  <a href={`/movies/${hit.objectID}`}>
   <article className="flex flex-col sm:flex-row gap-4 p-4 border border-gray-200 dark:border-gray-700 rounded-lg shadow hover:shadow-md transition bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-100">
     <Image
       src={hit.image_src}
       alt={hit.title}
       width={50}
       height={100}
-      className="h-auto object-cover rounded-md border border-gray-300 dark:border-gray-600"
+      className="hidden sm:block h-auto object-cover rounded-md border border-gray-300 dark:border-gray-600"
     />
     <div className="flex flex-col justify-between">
-      <h1 className="text-lg font-semibold text-gray-800 dark:text-gray-100">
+      <h1 className="sm:text-lg font-semibold text-gray-800 dark:text-gray-100">
         <Highlight attribute="data.title" hit={hit} />
       </h1>
-      <p className="text-sm text-gray-600 dark:text-gray-400">
+      <p className="hidden sm:block text-sm text-gray-600 dark:text-gray-400">
         <span className="font-medium">Genre:</span>{' '}
         <Highlight attribute="data.genre" hit={hit} />
       </p>
-      <p className="text-sm text-gray-600 dark:text-gray-400">
+      <p className="hidden sm:block text-sm text-gray-600 dark:text-gray-400">
         <span className="font-medium">Actors:</span>{' '}
         <Highlight attribute="data.actors" hit={hit} />
       </p>
     </div>
   </article>
+  </a>
 );
 
 
@@ -82,7 +84,7 @@ export function Header() {
                   root: '',
                   form: 'relative',
                   input:
-                    'w-full h-12 pl-10 pr-4 rounded-lg border border-gray-300 dark:border-gray-600 dark:bg-gray-900 text-gray-900 dark:text-white placeholder-gray-500 dark:placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500',
+                    'w-full h-12 pl-10 mt-4 pr-4 rounded-lg border border-gray-300 dark:border-gray-600 dark:bg-gray-900 text-gray-900 dark:text-white placeholder-gray-500 dark:placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500',
                   submit: 'hidden',
                 }}
               />
@@ -90,6 +92,7 @@ export function Header() {
                 className="w-5 h-5 text-gray-500 dark:text-gray-400 absolute left-3 top-1/2 -translate-y-1/2 pointer-events-none"
               />
             </div>
+              <RefinementList attribute="data.release" />
             <CustomHits />
 
           </div>
