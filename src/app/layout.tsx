6170060881f1c7b7fdfd57ab02100cfa54c7eabb
Sriyansh Gupta <sriyanshgupta28204@gmail.com>
2025-08-02 20:45:41 +0000

    make the website light theme with blue color contrast

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en" className="">
       <body
         className={cn(
           'min-h-screen bg-background font-body antialiased flex flex-col',
